e85437bdd7f11818f86ea7cffe92bcc798f82a3e
MaratXcode <kosjachok8@yandex.ru>
2024-09-11 16:10:55 +0300

    add progressView

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -55,7 +55,7 @@ struct ContentView: View {
             
             VStack{
                 
-
+                ProgressView(value: Float(result),total: 8 )
                 
                 Spacer()
                 
